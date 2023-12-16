a385b1336b8310aa3b7005d9c333c176a7f55a31
Nicholas <danielnick210@gmail.com>
2023-12-16 01:13:26 +0100

    creating a combo box

--- youtube_downloader/env/file.py
+++ youtube_downloader/env/file.py
@@ -6,7 +6,7 @@ import os
 #setting root window
 root = ctk.CTk()
 ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
+ctk.set_default_color_theme("dark-blue")
 
 #seting title
 root.title("Youtube Downloader!")
@@ -15,6 +15,26 @@ root.title("Youtube Downloader!")
 root.geometry("720,480")
 root.minsize(720, 480)
 root.maxsize(1080, 720)
+
+#create a frame to hold the content
+contentFrame = ctk.CTkFrame(root)
+contentFrame.pack(fill=ctk.BOTH, expand=True)
+
+
+#create a lable and the entry widget for the vidoe url
+urlLable = ctk.CTkLabel(contentFrame, text="Kindly enter the youtube url")
+entryUrl = ctk.CTkEntry(contentFrame, width=400, height=40)
+urlLable.pack(pady=("10p", "5p"))
+entryUrl.pack(pady=("10p", "5p"))
+
+#create a download button
+downloadBotton = ctk.CTkButton(contentFrame, text="Dowmload")
+downloadBotton.pack(pady=("10p", "5p"))
+
+#creats a resolution combo box
+resolutions =["720px", "360", "240"]
+resolutionVar = ctk.StringVar()
+resolutionComboBox = ctk.CTkComboBox(contentFrame, values=resolutions)
 
 #mainloop
 root.mainloop()
